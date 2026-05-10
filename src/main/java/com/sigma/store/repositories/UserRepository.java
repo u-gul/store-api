1a8a6c27a11db147344a0c71a12b3a9035a441ed
u-gul <ujjwal.dev17@gmail.com>
2026-05-10 07:46:11 +0000

    it now extends JpaRepository

--- src/main/java/com/sigma/store/repositories/UserRepository.java
+++ src/main/java/com/sigma/store/repositories/UserRepository.java
@@ -1,8 +1,8 @@
 package com.sigma.store.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sigma.store.entities.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
