2aca48b12b530e7bc165224a8122965e6b0b809d
u-gul <ujjwal.dev17@gmail.com>
2026-05-10 15:22:27 +0000

    changes

--- src/main/java/com/sigma/store/controllers/UserController.java
+++ src/main/java/com/sigma/store/controllers/UserController.java
@@ -1,20 +1,17 @@
 package com.sigma.store.controllers;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.sigma.store.dtos.UserDto;
 import com.sigma.store.entities.User;
 import com.sigma.store.mappers.UserMapper;
 import com.sigma.store.repositories.UserRepository;
-
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -25,8 +22,14 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
-    public List<UserDto> getAllUsers() {
-        return userRepository.findAll()
+    public List<UserDto> getAllUsers(
+            @RequestParam(required = false, defaultValue = "name") String sort) {
+
+        if (!Set.of("name", "email").contains(sort)) {
+            sort = "name";
+        }
+
+        return userRepository.findAll(Sort.by(sort))
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
