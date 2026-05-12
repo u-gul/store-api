0771911787cbde1c3f5ec4155940eac002b6f299
u-gul <ujjwal.dev17@gmail.com>
2026-05-12 20:00:43 +0530

    add user controller methods

--- src/main/java/com/sigma/store/controllers/UserController.java
+++ src/main/java/com/sigma/store/controllers/UserController.java
@@ -1,17 +1,32 @@
 package com.sigma.store.controllers;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.sigma.store.dtos.RegisterUserRequest;
+import com.sigma.store.dtos.UpdateUserRequest;
 import com.sigma.store.dtos.UserDto;
 import com.sigma.store.entities.User;
 import com.sigma.store.mappers.UserMapper;
 import com.sigma.store.repositories.UserRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Set;
+import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
@@ -23,7 +38,10 @@ public class UserController {
 
     @GetMapping
     public List<UserDto> getAllUsers(
+            @RequestHeader(required = false, name = "x-auth-token") String authToken,
             @RequestParam(required = false, defaultValue = "name") String sort) {
+
+        System.out.println(authToken);
 
         if (!Set.of("name", "email").contains(sort)) {
             sort = "name";
@@ -43,5 +61,41 @@ public class UserController {
         }
 
         return new ResponseEntity<>(userMapper.toDto(user), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody RegisterUserRequest request,
+            UriComponentsBuilder uriBuilder) {
+        User user = userMapper.toEntity(request);
+        userRepository.save(user);
+
+        UserDto userDto = userMapper.toDto(user);
+        URI uri = uriBuilder.path("/users/{id}").buildAndExpand(userDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(userDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(
+            @PathVariable(name = "id") Long id,
+            @RequestBody UpdateUserRequest updateUserRequest) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        userMapper.update(updateUserRequest, user);
+        userRepository.save(user);
+        return ResponseEntity.ok(userMapper.toDto(user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable(name = "id") Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        userRepository.delete(user);
+        return ResponseEntity.ok(null);
     }
 }
