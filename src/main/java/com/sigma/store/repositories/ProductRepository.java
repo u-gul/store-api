bfa83a13e8883cca9f88b7e1d6c54f36c4a6a242
u-gul <ujjwal.dev17@gmail.com>
2026-05-12 20:01:50 +0530

    changes

--- src/main/java/com/sigma/store/repositories/ProductRepository.java
+++ src/main/java/com/sigma/store/repositories/ProductRepository.java
@@ -1,8 +1,19 @@
 package com.sigma.store.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sigma.store.entities.Product;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @EntityGraph(attributePaths = "category")
+    List<Product> findByCategoryId(Byte categoryId);
+
+    @EntityGraph(attributePaths = "category")
+    @Query("Select p from Product p")
+    List<Product> findAllWithCategory();
 }
