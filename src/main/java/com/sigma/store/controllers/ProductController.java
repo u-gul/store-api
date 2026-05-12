b42e385ba99a927cf4cfffff0785205ca9c4cab4
u-gul <ujjwal.dev17@gmail.com>
2026-05-12 20:00:28 +0530

    add product controller methods

--- src/main/java/com/sigma/store/controllers/ProductController.java
+++ src/main/java/com/sigma/store/controllers/ProductController.java
@@ -2,10 +2,15 @@ package com.sigma.store.controllers;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sigma.store.dtos.ProductDto;
+import com.sigma.store.entities.Product;
 import com.sigma.store.mappers.ProductMapper;
 import com.sigma.store.repositories.ProductRepository;
 
@@ -18,7 +23,29 @@ public class ProductController {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
 
-    public List<ProductDto> getAllProducts() {
-        return productRepository.findAll().stream().map(productMapper::toDto).toList();
+    @GetMapping
+    public List<ProductDto> getAllProducts(
+            @RequestParam(name = "categoryId", required = false) Byte categoryId) {
+
+        List<Product> products;
+        if (categoryId != null) {
+            products = productRepository.findByCategoryId(categoryId);
+        } else {
+            products = productRepository.findAllWithCategory();
+        }
+        return products.stream().map(productMapper::toDto).toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable Long id) {
+        Product product = productRepository.findById(id).orElse(null);
+        // return new ProductDto(product.getId(), product.getName(), product.getPrice(),
+        // product.getDescription(),
+        // product.getCategory().getId());
+
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(productMapper.toDto(product));
     }
 }
