0e39ea0157c17071d218edbbf9665eaf9b156e6f
u-gul <ujjwal.dev17@gmail.com>
2026-05-12 20:01:42 +0530

    changes

--- src/main/java/com/sigma/store/mappers/UserMapper.java
+++ src/main/java/com/sigma/store/mappers/UserMapper.java
@@ -1,11 +1,18 @@
 package com.sigma.store.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
+import com.sigma.store.dtos.RegisterUserRequest;
+import com.sigma.store.dtos.UpdateUserRequest;
 import com.sigma.store.dtos.UserDto;
 import com.sigma.store.entities.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toDto(User user);
+
+    User toEntity(RegisterUserRequest registerUserRequest);
+
+    void update(UpdateUserRequest updateUserRequest, @MappingTarget User user);
 }
