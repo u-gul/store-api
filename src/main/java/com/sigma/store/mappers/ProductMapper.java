e7a49ef537edaf668ca357a4fddc351756b5b89c
u-gul <ujjwal.dev17@gmail.com>
2026-05-16 10:43:12 +0000

    update mapper

--- src/main/java/com/sigma/store/mappers/ProductMapper.java
+++ src/main/java/com/sigma/store/mappers/ProductMapper.java
@@ -2,6 +2,7 @@ package com.sigma.store.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import com.sigma.store.dtos.ProductDto;
 import com.sigma.store.entities.Product;
@@ -13,4 +14,7 @@ public interface ProductMapper {
     ProductDto toDto(Product product);
 
     Product toEntity(ProductDto productDto);
+
+    @Mapping(target = "id", ignore = true)
+    void update(ProductDto productDto, @MappingTarget Product product);
 }
