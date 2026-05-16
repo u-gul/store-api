405d6f768d5e81e7f9b8bd75479493e7e81859b9
u-gul <ujjwal.dev17@gmail.com>
2026-05-16 10:04:13 +0000

    add setter

--- src/main/java/com/sigma/store/dtos/ProductDto.java
+++ src/main/java/com/sigma/store/dtos/ProductDto.java
@@ -4,9 +4,11 @@ import java.math.BigDecimal;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class ProductDto {
     private Long id;
     private String name;
