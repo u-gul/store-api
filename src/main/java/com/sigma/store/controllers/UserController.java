3d9a7996244d23895563e5201359ab93f71a38d8
u-gul <ujjwal.dev17@gmail.com>
2026-05-17 02:00:29 +0000

    add exception handler

--- src/main/java/com/sigma/store/controllers/UserController.java
+++ src/main/java/com/sigma/store/controllers/UserController.java
@@ -7,7 +7,9 @@ import java.util.Set;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -118,5 +120,11 @@ public class UserController {
         userRepository.save(user);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationErrors(
+            MethodArgumentNotValidException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
     }
 }
