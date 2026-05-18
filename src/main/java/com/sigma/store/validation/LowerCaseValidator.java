3bd603fecdd2eb0ae51ea2ba7932c34df569dcba
u-gul <ujjwal.dev17@gmail.com>
2026-05-18 02:28:17 +0000

    Custom Exception Handler

---
+++ src/main/java/com/sigma/store/validation/LowerCaseValidator.java
@@ -0,0 +1,14 @@
+package com.sigma.store.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class LowerCaseValidator implements ConstraintValidator<LowerCase, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null)
+            return true;
+        return value.equals(value.toLowerCase());
+    }
+}
