68b21fcd1cc9b3c59f309b6486fd5b3cf21aa31d
u-gul <ujjwal.dev17@gmail.com>
2026-05-20 20:46:03 +0530

    changes

--- src/main/java/com/sigma/store/mappers/CartMapper.java
+++ src/main/java/com/sigma/store/mappers/CartMapper.java
@@ -1,10 +1,14 @@
 package com.sigma.store.mappers;
 
 import com.sigma.store.dtos.CartDto;
+import com.sigma.store.dtos.CartItemDto;
 import com.sigma.store.entities.Cart;
+import com.sigma.store.entities.CartItem;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
     CartDto toDto(Cart cart);
+
+    CartItemDto toDto(CartItem cartItem);
 }
