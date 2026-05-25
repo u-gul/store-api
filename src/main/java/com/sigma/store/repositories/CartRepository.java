45e7f37ddec3f305782ba690bf275b019a4e62fa
u-gul <ujjwal.dev17@gmail.com>
2026-05-25 13:19:39 +0000

    update

--- src/main/java/com/sigma/store/repositories/CartRepository.java
+++ src/main/java/com/sigma/store/repositories/CartRepository.java
@@ -1,9 +1,17 @@
 package com.sigma.store.repositories;
 
 import com.sigma.store.entities.Cart;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, UUID> {
+
+    @EntityGraph(attributePaths = "items.product")
+    @Query("Select c from Cart c where c.id = :cartId")
+    Optional<Cart> getCartWithItems(@Param("cartId") UUID cartId);
 }
