c727eeea11cdd576ebab6cf6dc6c2f10858de548
u-gul <ujjwal.dev17@gmail.com>
2026-05-25 13:19:36 +0000

    update

--- src/main/java/com/sigma/store/mappers/CartMapper.java
+++ src/main/java/com/sigma/store/mappers/CartMapper.java
@@ -9,7 +9,8 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
-    
+
+    @Mapping(target = "totalPrice", expression = "java(cart.getTotalPrice())")
     CartDto toDto(Cart cart);
 
     @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
