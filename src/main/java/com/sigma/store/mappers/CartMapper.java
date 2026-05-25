b7163cd7b340f85e73c885478d4153be368ec2e4
u-gul <ujjwal.dev17@gmail.com>
2026-05-25 08:13:37 +0000

    add totalPrice expression in the CartMapper

--- src/main/java/com/sigma/store/mappers/CartMapper.java
+++ src/main/java/com/sigma/store/mappers/CartMapper.java
@@ -5,10 +5,12 @@ import com.sigma.store.dtos.CartItemDto;
 import com.sigma.store.entities.Cart;
 import com.sigma.store.entities.CartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
     CartDto toDto(Cart cart);
 
+    @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
     CartItemDto toDto(CartItem cartItem);
 }
